d5b953de904823d1c82e08892597a3072727d20c
raghu <raghumurukutla96@gmail.com>
2026-04-14 21:15:30 +0530

    01 vriable script

---
+++ 03-conservation.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+#!/bash/bin
+Person1=$1
+Person2=$2
+echo "$Person1:: Hey Musk, How are you?"
+echo "$Person2:: Hello $Person1, I am fine. How are you doing"
+echo "$Person1: I am fine too. What's up?"
+echo "$Person2:: Nohting, just going to Mars now, will you come?"
+echo "$Person1:: Sorry, you carry on! I will come once you come back".
