463b294cce0101b8c9b1b53666f564c20f1bf814
raghu <raghumurukutla96@gmail.com>
2026-04-15 21:58:24 +0530

    added

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-
-
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-   echo "please run as a root user"
+   echo "please run as a root user"  
    exit 1
 else 
    echo "logged as admin, please proceed "
 fi
-VALIDATE(){
-if [ $1 -ne 0 ]
-then
-   echo "installing $2 in the machine" 
 
-   dnf install $2 -y
+VALIDATE(){
+
+if [ $1 -eq 0 ]
+then 
+   echo "installing $2 is sucessful"
 else
-   echo "$2 is already installed in the machine"
+   echo "installing $2 is failed"
    exit 1
 fi
 }
 
+dnf list installed mysql 
 
-dnf list installed $2
+if [ $? -ne 0 ]
+then
+   echo "installing mysql in the machine" 
 
-VALIDATE $? "$2"
+   dnf install mysql -y
+   VALIDATE $? "$2"
+else
+   echo "mysql is already installed in the machine" 
+   
+fi
