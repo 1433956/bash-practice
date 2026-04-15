391aead9b6d5edd538acbf10c85b8ffbbc1c8904
raghu <raghumurukutla96@gmail.com>
2026-04-15 22:03:47 +0530

    added

--- 10-functions.sh
+++ 10-functions.sh
@@ -28,7 +28,7 @@ then
    echo "installing mysql in the machine" 
 
    dnf install $2 -y
-   VALIDATE $? "$2"
+   VALIDATE $? "mysql"
 else
    echo "mysql is already installed in the machine" 
    
