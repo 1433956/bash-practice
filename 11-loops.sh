dde4835e0248ce8eb8140a3655773c493d334fe9
raghu <raghumurukutla96@gmail.com>
2026-04-21 06:59:48 +0530

    commiting code

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 LOG_FOLDER="usr/log/shell-logs"
 mkdir -p $LOG_FOLDER
-LOG_FILENAME=$(echo $0 |cut -d "." f1)
+LOG_FILENAME=$(echo $0 |cut -d "." -f1)
 LOG_FILE="$LOG_FOLDER/$LOG_FILENAME.log"
 
 R="\e[31m"
