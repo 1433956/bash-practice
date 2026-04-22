3251cbbc3a88a9cb7bd73760e2d1a2010564543d
raghu <raghumurukutla96@gmail.com>
2026-04-22 07:02:32 +0530

    adding loop script

--- 11-loops.sh
+++ 11-loops.sh
@@ -9,14 +9,38 @@ G="\e[32m"
 Y="\e[33m"
 W="\e[0m"
 USERID=$(id -u)
-
+PACKAGES=("mysql" "nginx" "nodejs")
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R please login user as a root user::$USERID $W" $>> tee -a LOG_FILE
+    echo -e "$R please login user as a root user::$USERID $W"  &>> $LOG_FILE
     exit 1
 else 
     echo -e "$G loged user as a root user::$USERID $W" | tee -a $LOG_FILE
 fi
 
-  
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2 is ...failure $W" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G $2 is ...Success $W" | tee -a $LOG_FILE
+    fi
+
+}
+
+#check PACKAGES is installed or not   if not install package 
+ for package in ${PACKAGES[@]}
+ do 
+   dnf list installed  $package
+   if [ $? -eq 0 ]
+   then 
+      echo -e "$G installed in the machine $W"  &>> $LOG_FILE
+   else
+      echo -e  "$R Not  installed in the machine installing the $2 $W"  &>> $LOG_FILE
+      dnf install $package -y &>> $LOG_FILE
+      VALIDATE $? "$package" 
+    fi
+    
+ done 
 
